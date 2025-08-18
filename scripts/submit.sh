cb8211d1981b90c4b89df532c39b78f4a76f4492
Vedant Puri <vedantpuri@gmail.com>
2025-08-18 09:12:38 -0400

    submission script

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -27,7 +27,7 @@
 # #SBATCH --partition=preempt
 # #SBATCH --qos=preempt_qos
 # #SBATCH --gres=gpu:1
-# #SBATCH --time=18:00:00
+# #SBATCH --time=24:00:00
 # #SBATCH --job-name=FLARE
 
 #============================#
@@ -41,12 +41,14 @@ cd /project/community/vedantpu/FLARE.py
 # Run interactively with
 # $ srun --pty --overlap --jobid <job_id> bash
 #============================#
-# sleep 12h
+# sleep 24h
 #============================#
 
+# uv run python -m pdebench --train true \
+#     --dataset darcy --model_type 3 --exp_name model_3_darcy
+#======================================================================#
 DATASET=darcy
 EPOCH=500
-LR=1e-3
 BATCH_SIZE=2
 WEIGHT_DECAY=1e-5
 
@@ -55,18 +57,12 @@ NUM_CHANNELS=64
 NUM_LATENTS=256
 NUM_HEADS=8
 
-NUM_LAYERS_IN_OUT_PROJ=2 # -1
-NUM_LAYERS_KV_PROJ=3
-NUM_LAYERS_MLP=3
-
-MIXED_PRECISION=false
-# MIXED_PRECISION=true
-
-uv run python -m pdebench --dataset ${DATASET} --train true --model_type 2 --mixed_precision ${MIXED_PRECISION} \
+uv run python -m pdebench --dataset ${DATASET} --train true --model_type 2 \
     --epochs ${EPOCH} --weight_decay ${WEIGHT_DECAY} --batch_size ${BATCH_SIZE} \
     --channel_dim ${NUM_CHANNELS} --num_latents ${NUM_LATENTS} --num_blocks ${NUM_BLOCKS} --num_heads ${NUM_HEADS} \
-    --num_layers_in_out_proj ${NUM_LAYERS_IN_OUT_PROJ} --num_layers_kv_proj ${NUM_LAYERS_KV_PROJ} --num_layers_mlp ${NUM_LAYERS_MLP} \
-    --seed 0 --exp_name model_2_${DATASET}_B_${NUM_BLOCKS}_L_${NUM_LATENTS}_H_${NUM_HEADS}
+    --seed 0 --exp_name model_2_${DATASET}_B_${NUM_BLOCKS}_C_${NUM_CHANNELS}_M_${NUM_LATENTS}_H_${NUM_HEADS}
+
+#======================================================================#
 
 #============================#
 wait
